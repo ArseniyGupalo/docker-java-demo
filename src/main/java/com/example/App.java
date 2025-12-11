1492fecd641716f19a52cebc602ae4780c2f9cbe
SonCrato <arseniy26012004@gmail.com>
2025-12-11 11:41:09 +0200

    New output

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -21,11 +21,9 @@ public class App {
     static class RootHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String version = "1.0";
-            String response = "Hello from Java in Docker ";
+            String response = "Hello from Java in Docker!";
             exchange.sendResponseHeaders(200, response.getBytes().length);
             try (OutputStream os = exchange.getResponseBody()) {
-                response = response + version;
                 os.write(response.getBytes());
             }
         }
